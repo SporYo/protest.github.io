2093ae1d5d0e0ad34971eec53a1047eaf9ab25d0
Sergey Kostin <sporeyo89@gmail.com>
2019-10-02 20:12:49 +0300

    added new blocks

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+// меню бургер
 (function($){
     $(function() {
       $('.menu__icon').on('click', function() {
@@ -5,3 +6,30 @@
       });
     });
   })(jQuery);
+
+  // слайдер
+  $('.slider-center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
