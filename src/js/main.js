8eb065fcc4da38d24a1fb8d84153ce6cd722ab01
Sergey Kostin <sporeyo89@gmail.com>
2019-10-03 20:22:44 +0300

    Добавил блоки

--- src/js/main.js
+++ src/js/main.js
@@ -33,3 +33,40 @@
       }
     ]
   });
+
+  // maskinput
+
+  $(function(){
+    $(".phone_mask").mask("8(999) 999-9999");
+  });
+
+  // плавный якорь
+
+  $(document).ready(function(){
+    $("main").on("click",".arr-down", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+function replace() {
+  document.querySelector(".right").style.display="block";
+  document.querySelector(".right2").style.display="none";
+  document.querySelector(".right3").style.display="none";
+}
+function replace2() {
+  document.querySelector(".right").style.display="none";
+  document.querySelector(".right2").style.display="block";
+  document.querySelector(".right3").style.display="none";
+}
+function replace3() {
+  document.querySelector(".right").style.display="none";
+  document.querySelector(".right2").style.display="none";
+  document.querySelector(".right3").style.display="block";
+}
